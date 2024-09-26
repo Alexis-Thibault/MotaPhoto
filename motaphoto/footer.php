18b6b9e219b0b26fc83dcc916732eda86783e811
Alexis <alexis.thibault45150@gmail.com>
2024-09-26 18:10:31 +0200

    finalisation du footer avec responsive

--- motaphoto/footer.php
+++ motaphoto/footer.php
@@ -1,3 +1,9 @@
+    <footer>
+      <a href="<?php echo site_url('/mentions-legales'); ?>">Mentions Légales</a>
+      <a href="<?php echo site_url('/vie-privee'); ?>">Vie privée</a>
+      <a href="#">Tous droits réservés</a>
+    </footer>
 
+    <?php wp_footer(); ?> <!-- Chargement des scripts de fin de page -->
   </body>
 </html>

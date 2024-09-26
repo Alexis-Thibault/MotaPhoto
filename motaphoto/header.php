28d0c75506b4ce28bd78bb1711021c3134896bc4
Alexis <alexis.thibault45150@gmail.com>
2024-09-26 17:51:18 +0200

    correction apporté sur le header

--- motaphoto/header.php
+++ motaphoto/header.php
@@ -1,23 +1,25 @@
 <!doctype html>
 <html lang="fr">
-  <head>
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>MotaPhoto</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" />
     <?php wp_head(); ?>
-  </head>
-  <body>
+</head>
+<body>
 
-  <nav>
-    <div>
-      <a href="<?php echo home_url(); ?>">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo.png" alt="Logo">
-      </a>  
-    </div>
-    <div>
-      <a href="#">Accueil</a>
-      <a href="#">À propos</a>
-      <a href="#">Contact</a>
-    </div>
-  </nav>
+<header>
+    <nav>
+        <div>
+            <a href="<?php echo home_url(); ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo.png" alt="Logo">
+            </a>
+        </div>
+        <div>
+            <a href="<?php echo home_url(); ?>">Accueil</a>
+            <a href="<?php echo home_url('/about'); ?>">À propos</a>
+            <a href="<?php echo home_url('/contact'); ?>">Contact</a>
+        </div>
+    </nav>
+</header>

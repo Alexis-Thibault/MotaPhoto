45597dfe4a49aa5bae390226b10808dc4707dfd0
Alexis <alexis.thibault45150@gmail.com>
2024-09-27 13:55:46 +0200

    ajout de l'animation sur la modal

--- motaphoto/assets/scripts/modal.js
+++ motaphoto/assets/scripts/modal.js
@@ -1,25 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     const modal = document.getElementById("contactModal");
     const openModalBtn = document.querySelector('a[href="#contact"]');
+    
     // Fonction pour ouvrir la modale
     const openModal = function(e) {
         e.preventDefault();
+        modal.classList.remove("fade-out");
         modal.classList.add("visible");
     };
 
     // Fonction pour fermer la modale
-    const closeModalFn = function() {
-        modal.classList.remove("visible");
+    const closeModal = function() {
+        modal.classList.add("fade-out"); 
+        setTimeout(() => modal.classList.remove("visible"), 500);
     };
 
-    // Ouvrir la modale au clic sur le lien "Contact"
+    // Ouvrir la modale
     openModalBtn.addEventListener("click", openModal);
 
-    // Fermer la modale si on clique en dehors du contenu de la modale
+    // Fermer la modale en cliquant en dehors
     window.addEventListener("click", function(e) {
-        // Vérifie si l'élément cliqué est bien la modale (et non son contenu)
         if (e.target === modal) {
-            closeModalFn();
+            closeModal();
         }
     });
 });

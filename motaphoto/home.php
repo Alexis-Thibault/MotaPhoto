6bb6441bba740a5e88c3bd71192c693dd6f06a82
Alexis <alexis.thibault45150@gmail.com>
2024-10-11 15:59:29 +0200

    ajout des photos dans la page d'accueil

--- motaphoto/home.php
+++ motaphoto/home.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <?php 
-// Récupérer les images de la médiathèque
+// Récupérer les images de la médiathèque pour le background aléatoire
 $args = array(
     'post_type'      => 'attachment',
     'post_mime_type' => 'image',
@@ -24,6 +24,30 @@ if ($images) {
 
 <div class="header-home" style="background-image: url('<?php echo $image_url; ?>');">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/title-header-home.png" alt="Titre">
+</div>
+
+<!-- Section où tu affiches les blocs d'images du CPT "photos" -->
+<div class="photo-container container">
+    <?php 
+    // Créer une nouvelle requête pour récupérer les photos du CPT "photos"
+    $args = array(
+        'post_type' => 'photos', // Type de contenu personnalisé
+        'posts_per_page' => -1, // Récupérer toutes les photos
+    );
+    $photo_query = new WP_Query($args);
+
+    if ($photo_query->have_posts()):
+        while ($photo_query->have_posts()): $photo_query->the_post();
+        
+            // Passer l'image et les informations nécessaires à `photo_block.php`
+            get_template_part('templates_part/photo_block');
+        
+        endwhile;
+        wp_reset_postdata(); // Réinitialiser la requête
+    else:
+        echo '<p>Aucune photo trouvée.</p>';
+    endif;
+    ?>
 </div>
 
 <?php get_footer(); ?>
